103ea64106a548879a391d92db03b0563e25c789
mahfuz091 <engmahfuz14@gmail.com>
2023-06-09 21:42:05 +0600

    token send to backend

--- src/Providers/Authprovider.jsx
+++ src/Providers/Authprovider.jsx
@@ -56,7 +56,7 @@ const Authprovider = ({ children }) => {
       if (currentUser) {
         axios.post('http://localhost:5000/jwt', { email: currentUser.email })
           .then(data => {
-            // console.log(data.data.token)
+
             localStorage.setItem('access-token', data.data.token)
             setLoading(false);
           })
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,11 +4,14 @@ import { useQuery } from "@tanstack/react-query";
 
 const useCart = () => {
   const { user } = useAuth();
+  const token = localStorage.getItem('access-token');
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["cart", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/carts?email=${user?.email}`
+        `http://localhost:5000/carts?email=${user?.email}`, {
+        headers: { authorization: `bearer ${token}` }
+      }
       );
       return res.json();
     },
