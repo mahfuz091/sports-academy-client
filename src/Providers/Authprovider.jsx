33c6541f25c3dcb600a82c46ba8c8e2ca158596b
mahfuz091 <engmahfuz14@gmail.com>
2023-06-09 21:11:51 +0600

    jwt access token set localstorage

--- src/Providers/Authprovider.jsx
+++ src/Providers/Authprovider.jsx
@@ -4,13 +4,14 @@ import {
   createUserWithEmailAndPassword,
   getAuth,
   onAuthStateChanged,
-  sendPasswordResetEmail,
+
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -52,7 +53,18 @@ const Authprovider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       console.log("current user", currentUser);
-      setLoading(false);
+      if (currentUser) {
+        axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+          .then(data => {
+            // console.log(data.data.token)
+            localStorage.setItem('access-token', data.data.token)
+            setLoading(false);
+          })
+      }
+      else {
+        localStorage.removeItem('access-token')
+      }
+
     });
     return () => {
       return unsubscribe();
