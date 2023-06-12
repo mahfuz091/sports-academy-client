817151b667da71ea011fee36c7bd7f933e7777ee
mahfuz091 <engmahfuz14@gmail.com>
2023-06-12 20:49:02 +0600

    added aos package

---
+++ src/Pages/Home/WhatWeDo/AnimateTextWord.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const AnimateTextWord = ({ text }) => {
+    const words = text.split(' ')
+    const container = {
+        hidden: { opacity: 0 },
+        visible: (i = 1) => ({
+            opacity: 1,
+            transition: { staggerChildren: 0.12, delayChildren: 0.04 * i }
+        }),
+
+    };
+    const child = {
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                type: "spring",
+                damping: 12,
+                stiffness: 100,
+            },
+            hidden: {
+                opacity: 0,
+                y: 20,
+                transition: {
+                    type: "spring",
+                    damping: 12,
+                    stiffness: 100,
+                },
+
+            }
+        }
+    }
+    return (
+        <motion.div varients={container} initial='hidden' animate='visible'>
+            {words.map((word, index) => <motion.span varients={child} style={{ marginRight: '5px' }} key={index}>{word}</motion.span>)}
+        </motion.div>
+    );
+};
+
+export default AnimateTextWord;
--- src/Pages/Home/WhatWeDo/WhatWeDo.jsx
+++ src/Pages/Home/WhatWeDo/WhatWeDo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import anime from 'animejs/lib/anime.es.js';
+
 import "./WhatWeDo.css";
 import img1 from "../../../assets/Whatwedo/w-1.jpg";
 import img2 from "../../../assets/Whatwedo/w-2.jpg";
@@ -7,23 +7,30 @@ import img3 from "../../../assets/Whatwedo/w-3.webp";
 import img4 from "../../../assets/Whatwedo/w-4.jpg";
 import img5 from "../../../assets/Whatwedo/w-5.webp";
 import img6 from "../../../assets/Whatwedo/w-6.webp";
+import AnimateTextWord from "./AnimateTextWord";
+
 
 
 const WhatWeDo = () => {
-  // anime({
-  //   targets: 'div',
-  //   translateX: 250,
-  //   rotate: '1turn',
-  //   backgroundColor: '#FFF',
-  //   duration: 800
-  // });
+
   return (
     <div >
+
       <div className='lg:flex items-start justify-between lg:gap-20 px-10 lg:px-32 mb-24'>
-        <h2 className='text-5xl font-semibold '>
-          What We <span className='text-[#dc1d24]'>Do</span>
-        </h2>
-        <p className='text-base lg:w-1/2 mt-5 lg:mt-0'>
+
+        <div
+          data-aos='fade-right'
+          data-aos-easing='linear'
+          data-aos-duration='1500'
+        >
+          <h2 className='text-5xl font-semibold '>
+            What We <span className='text-[#dc1d24]'>Do</span>
+
+          </h2>
+        </div>
+        <p data-aos='fade-left'
+          data-aos-easing='linear'
+          data-aos-duration='1500' className='text-base lg:w-1/2 mt-5 lg:mt-0'>
           The Sports Academy provides students with opportunities to be
           physically active while increasing social awareness and drive to max
           potential. We place specific attention on providing athletes with life
@@ -32,7 +39,8 @@ const WhatWeDo = () => {
         </p>
       </div>
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-10 lg:px-0 '>
-        <div class='image'>
+        <div class='image'
+        >
           <img class='image__img w-96 h-52 rounded-xl' src={img1} alt='' />
           <div class='image__overlay image__overlay--primary'>
             <div class='image__title'>Basket Ball Camp</div>
